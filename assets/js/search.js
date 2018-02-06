48b4f3b21df56aaa0478e1f67eb658888e6a63d8
Phodal HUANG <h@phodal.com>
2018-02-06 21:33:11 +0800

    Updates

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,3 @@
 ﻿var typedoc = typedoc || {};
             typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"128":"Class","512":"Constructor"},"rows":[{"id":0,"kind":128,"name":"Mus","url":"classes/mus.html","classes":"tsd-kind-class tsd-has-type-parameter"},{"id":1,"kind":512,"name":"constructor","url":"classes/mus.html#constructor","classes":"tsd-kind-constructor tsd-parent-kind-class","parent":"Mus"}]};
+            typedoc.search.data = {"kinds":{"64":"Function","128":"Class","512":"Constructor"},"rows":[{"id":0,"kind":128,"name":"Mus","url":"classes/mus.html","classes":"tsd-kind-class tsd-has-type-parameter"},{"id":1,"kind":512,"name":"constructor","url":"classes/mus.html#constructor","classes":"tsd-kind-constructor tsd-parent-kind-class","parent":"Mus"},{"id":2,"kind":64,"name":"Transition","url":"globals.html#transition","classes":"tsd-kind-function"}]};
